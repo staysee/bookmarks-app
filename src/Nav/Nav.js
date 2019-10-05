1d2599853bbdf1355d51ddf08b88b9553d524647
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-10-05 10:47:08 -0700

    added router

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Nav(props) {
   return (
     <nav className='Nav'>
-      <button onClick={() => props.clickPage('list')}>
+      {/* <button onClick={() => props.clickPage('list')}> */}
+      <Link to={'/'}>
         Bookmark List
-      </button>
+      </Link>
+      {/* </button> */}
       {' '}
-      <button onClick={() => props.clickPage('add')}>
+      {/* <button onClick={() => props.clickPage('add')}> */}
+      <Link to={'/add-bookmark'}>
         Add Bookmark
-      </button>
+      </Link>
+      {/* </button> */}
     </nav>
   );
 }
