3648c7fa88c3eb3798d4fddcae053c5b57460165
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-10-22 20:06:20 -0700

    validated the URL

--- src/BookmarkItem/BookmarkItem.js
+++ src/BookmarkItem/BookmarkItem.js
@@ -79,7 +79,26 @@ BookmarkItem.defaultProps = {
 
 BookmarkItem.propTypes = {
 	title: PropTypes.string.isRequired,
-	url: PropTypes.string.isRequired,
+	url: (props, propName, componentName) => {
+		//get the value of the prop
+		const prop = props[propName];
+
+		//do the isRequired check
+		if (!prop) {
+			return new Error(`${propName} is required in ${componentName}. Validation failed`);
+		}
+
+		//check the type
+		if (typeof prop != 'string') {
+			return new Error(`Invalid prop, ${propName} is expected to be a string in ${componentName}. ${typeof prop} found.`);
+		}
+
+		//do the custom check here
+		//using a simple regex
+		if (prop.length < 5 || !prop.match(new RegExp(/^https?:\/\//))) {
+			return new Error(`Invalid prop, ${propName} must be min length 5 and begin http(s)://. Validation failed.`);
+		}
+	},
 	rating: PropTypes.number,
 	description: PropTypes.string
 }
