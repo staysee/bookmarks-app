eb4f04bfb4d038b02dcd42bcb6e8399555a5b106
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-10-30 17:40:21 -0700

    tests passed, otw to deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$lQzNMiMpqlwsP3yvLr2Ed.UXxhKH9fni3gKuoj/gsZucPd83wJCzm',
+  API_KEY: process.env.REACT_APP_API_KEY
 }
